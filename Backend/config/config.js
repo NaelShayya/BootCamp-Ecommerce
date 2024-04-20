716f0c82e284f420d07e11c158411f708aec7a94
Nael Shayya <naelshayya@gmail.com>
2024-04-20 09:12:19 +0300

    first fix

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,14 +1,12 @@
+import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-
-    /*ADD DATA ONCE*/
-   // User.insertMany(users);
-   // Post.insertMany(posts);
 })
 .catch((error) => console.log(`Error connecting to MongoDB:`, error));
