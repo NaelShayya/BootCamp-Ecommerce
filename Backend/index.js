294d9880919f637d51292dcb8dcc847c1d301d63
sabine-aa <sabineabouammar1@gmail.com>
2024-04-20 11:13:17 +0300

    Role model

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
-import './config/config.js'
+import "./config/config.js";
+import Role from "./models/Role.js";
 
 const app = express();
 app.use(express.json());
+
+console.log("role", Role);
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,46 +1,44 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    first_name: {
-        type: String,
-        required: true
-    },
-    last_name: { 
-        type: String,
-         required: true
-         },
-    email: {
-         type: String,
-          required: true,
-           unique: true
-         },
-    password: { 
-        type: String,
-        required: true
-     },
-    role_id: {
-         type: mongoose.Schema.Types.ObjectId, 
-         ref: 'Role',
-          required: true
-         },
-    created_at: {
-         type: Date,
-         default: Date.now
-         },
-    modified_at: { 
-        type: Date,
-         default: Date.now
-         },
-    country_id: { 
-        type: mongoose.Schema.Types.ObjectId,
-         ref: 'Country',
-          required: true
-         }
-}); 
-
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  modified_at: {
+    type: Date,
+    default: Date.now,
+  },
+  country_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Country",
+    required: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
-
 
 export default User;
