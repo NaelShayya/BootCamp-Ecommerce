9905748a924c96bc938535b4b3064b99a5112288
Nael Shayya <naelshayya@gmail.com>
2024-05-03 11:51:15 +0300

    frontend setting up and router

--- Frontend/tutorial-website/src/pages/Home.jsx
+++ Frontend/tutorial-website/src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div className="n">
-    <div>Home</div>
-  
+      <div>Home</div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
---
+++ Frontend/tutorial-website/src/pages/noPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function noPage() {
+  return (
+    <div>404</div>
+  )
+}
+
+export default noPage
