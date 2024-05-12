39ab5eb91f0f2df49da5543496900b4f39ef0599
unknown <razanbittar57@gmail.com>
2024-05-12 22:44:02 +0300

    Corrections

--- Frontend/tutorial-website/src/pages/Layout.jsx
+++ Frontend/tutorial-website/src/pages/Layout.jsx
@@ -1,43 +1,54 @@
-
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 import SideNav from "../components/navbar";
-
 const Layout = () => {
-  
+  const handleLogout = () => {
+    sessionStorage.removeItem("token");
+    window.location.href = "/";
+  };
 
- return (
+  // Check if the user is authenticated based on the presence of the token
+  const isAuthenticated = sessionStorage.getItem("token") !== null;
+
+  return (
     <>
       <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
-          </li>
-          <li>
-            <Link to="/explore">Explore</Link>
-          </li>
-          <li>
-            <Link to="/course">Course</Link>
-          </li>
-          <li>
-            <button onClick={handleLogout}>Logout</button>
-          </li>
+          {!isAuthenticated && (
+            <>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+            </>
+          )}
+          {isAuthenticated && (
+            <>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+              <li>
+                <Link to="/explore">Explore</Link>
+              </li>
+              <li>
+                <Link to="/course">Course</Link>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
-     
-      <SideNav/>
-    
-      <Outlet />
 
+      <SideNav />
+
+      <Outlet />
     </>
   );
 };
