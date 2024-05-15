f1f2229acf05ef038e4214ac4321f08f52d80f29
sabine-aa <sabineabouammar1@gmail.com>
2024-05-16 01:05:10 +0300

    home changes

--- Frontend/tutorial-website/src/App.js
+++ Frontend/tutorial-website/src/App.js
@@ -1,30 +1,29 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Cart from "./pages/Cart";
-import Explore from "./pages/Explore"
-import Course from "./pages/Course"
-import Layout from "./pages/Layout"
-import NoPage from "./pages/noPage"
+import Explore from "./pages/Explore";
+import Course from "./pages/Course";
+import Layout from "./pages/layout/Layout";
+import NoPage from "./pages/noPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-      
-        <Route path="Cart" element={<Cart />} />
-        <Route path="Explore" element={<Explore />} />
-        <Route path="Course" element={<Course />} />
-        <Route path="*" element={<NoPage />} />
-      </Route>
-      <Route path="Login" element={<Login />} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route path="Cart" element={<Cart />} />
+          <Route path="Explore" element={<Explore />} />
+          <Route path="Course" element={<Course />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+        <Route path="Login" element={<Login />} />
         <Route path="Signup" element={<Signup />} />
-    </Routes>
-  </BrowserRouter>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
