655324e836b5f1ab7453c90391a4c6d898a4cd19
unknown <razanbittar57@gmail.com>
2024-05-18 02:30:22 +0300

    Changes

--- Frontend/tutorial-website/src/pages/Cart/Cart.jsx
+++ Frontend/tutorial-website/src/pages/Cart/Cart.jsx
@@ -116,7 +116,7 @@ const CheckoutForm = () => {
                 <div key={orderIndex} className="order-item">
                   <h3>Order #{orderIndex + 1}</h3>
                   {order.products.map((product, productIndex) => (
-                    <div key={productIndex} className="product">
+                    <div key={productIndex} className="product-cards">
                       <img
                         src={product.product_image}
                         alt="Product"
